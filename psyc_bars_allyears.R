56d15a55f07348c81089544b06cc2209ffc986c1
Bailey <baileypiggott@gmail.com>
2015-08-07 13:06:19 -0400

    Update colours to be consistent with engineering

--- psyc_bars_allyears.R
+++ psyc_bars_allyears.R
@@ -10,16 +10,14 @@ library(grid)
 
 psyc = read.csv("Psych_ALL_YEARS.csv")
 colnames(psyc)[2] <- "year"
-psyc$year <- as.factor(as.character(psyc$year))# convert year column to factor
+psyc$year <- as.factor(as.character(psyc$year))# convert year column to factor to plot in order
 
 #labels for VALUE criteria
 criteria <- c("Explanation\nof Issues",  "Influence of\nContext and\nAssumptions", "Student's\nPosition", "Conclusions", "Define\nProblem", "Identify\nStrategies","Propose\nSolutions", "Evaluate\nPotential\nSolutions", "Context and\nPurpose for\nWriting", "Content\nDevelopment", "Genre and\nDisciplinary\nConventions",  "Syntax and\nMechanics")
 
 # create column to define colours based on value criteria
 define_colour <- data.frame(define_colour = c('a','b','c','a','b','c', 'a','b','c','a','b','c','a','b','c','d','e','f','d','e','f','d','e','f','d','e','f', 'g','h','j', 'g','h','j', 'g','h','j', 'g','h','j', 'g','h','j')) # a,b,c = greens; d,e,f = oranges ; g,h,j = blues
-
 psyc <- bind_cols(psyc, define_colour)
-
 psyc$define_colour <- factor(psyc$define_colour, levels = c("a", "d", "g", "b","e","h","c","f", "j"))
 
 # LEGEND LABELS -----------------------------------------------------------
@@ -42,12 +40,12 @@ dodge <- position_dodge(width=0.5) # because bars and error bars will be differe
 ggplot(
   data = psyc, 
   aes(x = X, y = Mean, fill = define_colour)
-)+
+  )+
   geom_bar(stat = "identity",position = dodge, width = 0.5) + 
   geom_errorbar(
     aes(ymax = Mean + Std..Error, ymin = Mean - Std..Error),
     position = dodge, width = 0.25
-  ) + 
+    ) + 
   coord_cartesian(ylim = c(0, 4.4)) +
   scale_x_discrete(labels = criteria) +
   theme(
@@ -69,17 +67,17 @@ ggplot(
     x = "Learning Outcome", y = "Average Rubric Level"
     ) +
   scale_fill_manual(
-    values = c('#99e6a2', '#ffc499','#99c4f6', '#33cc44', '#ff8833','#3388ee', '#1f7a29','#cc5200' ,'#1f528f'),
+    values = c('#99c4f6','#99e6a2', '#ffc499', '#3388ee','#33cc44', '#ff8833','#1f528f', '#1f7a29','#cc5200' ),
     name = "Legend",
     labels = legend_labels
-  ) +
+    ) +
   annotate( # add labels for CLA mastery levels
     "text", 
     fontface = "bold", 
     size = 5,
     x = c(3, 7.5, 12), y = 4.2, 
     label = c( "Critical Thinking","Problem Solving" ,"Written Communication"), 
-    colour = c("#33CC44","#FF8833", "#3388EE")
-  )
+    colour = c("#3388EE", "#33CC44","#FF8833")
+    )
 
 
